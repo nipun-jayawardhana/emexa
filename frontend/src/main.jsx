b0fa4d0134119d16434b109a3a4d50e9d5c92a32
Tharindu Jayakody <140710028+atssamadhi@users.noreply.github.com>
2025-11-12 22:17:48 +0530

    fix: fix routing configuration

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,19 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router';
+import './index.css';
+import Home from './pages/Home';
+import SecondPage from './pages/SecondPage';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+   <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/second" element={<SecondPage />} />
+            {/* add protected routes */}
+            <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+    </BrowserRouter>
+  </StrictMode>
 )
