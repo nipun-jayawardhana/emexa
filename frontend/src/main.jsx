da2225d00375598fca7a2b8916617330e73f897f
Tharindu Jayakody <140710028+atssamadhi@users.noreply.github.com>
2025-11-13 01:26:17 +0530

    feat: change home page into app

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,14 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router';
 import './index.css';
-import Home from './pages/Home';
 import SecondPage from './pages/SecondPage';
+import App from './App';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
    <BrowserRouter>
         <Routes>
-            <Route path="/" element={<Home />} />
+            <Route path="/" element={<App />} />
             <Route path="/second" element={<SecondPage />} />
             {/* add protected routes */}
             <Route path="*" element={<Navigate to="/" replace />} />
