eb24b87fb3ea6e740c80b04ee7fc9b4de9b14a6a
Kavindi-Kinkini <10952821@students.plymouth.ac.uk>
2025-11-14 01:38:42 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,6 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router';
 import './index.css';
 import SecondPage from './pages/SecondPage';
-import UserMgt from './pages/usermgt';
 import App from './App';
 
 createRoot(document.getElementById('root')).render(
@@ -13,7 +12,6 @@ createRoot(document.getElementById('root')).render(
         <Routes>
             <Route path="/" element={<App />} />
             <Route path="/second" element={<SecondPage />} />
-            <Route path="/users" element={<UserMgt />} />
             {/* add protected routes */}
             <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
