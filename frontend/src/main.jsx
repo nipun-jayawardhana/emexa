be7c5a5b751afbdc9860d45bb49d0bc7e830afe9
Kavindi-Kinkini <10952821@students.plymouth.ac.uk>
2025-11-14 10:36:12 +0530

    Updated

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,20 +1,13 @@
-
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
-import SecondPage from './pages/SecondPage';
 import App from './App';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-   <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<App />} />
-            <Route path="/second" element={<SecondPage />} />
-            {/* add protected routes */}
-            <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes>
+    <BrowserRouter>
+      <App />
     </BrowserRouter>
   </StrictMode>
 )
