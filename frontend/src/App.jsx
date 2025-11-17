a707512ffe425808f3426321449c228b271605a7
nipun-jayawardhana <nipun.madhushanka1999@gmail.com>
2025-11-17 16:07:13 +0530

    fix alignment auth pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,19 @@
-import { Routes, Route, Navigate } from 'react-router-dom';
-import LandingPage from './pages/LandingPage';
-import StudentDashboard from './pages/StudentDashboard';
+import { Routes, Route, Navigate } from "react-router-dom";
+import Login from "./pages/Login"; // Import Login page
+import LandingPage from "./pages/LandingPage";
+import StudentDashboard from "./pages/StudentDashboard";
 
 export default function App() {
   return (
     <Routes>
-      {/* Landing Page Route */}
-      <Route path="/" element={<LandingPage />} />
-      
-      {/* Student Dashboard Route */}
+      {/* Default route points to Login page */}
+      <Route path="/" element={<Login />} />
+
+      {/* Other routes */}
+      <Route path="/landing" element={<LandingPage />} />
       <Route path="/student-dashboard" element={<StudentDashboard />} />
-      
-      {/* Redirect unknown routes to landing page */}
+
+      {/* Redirect unknown routes to login page */}
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
