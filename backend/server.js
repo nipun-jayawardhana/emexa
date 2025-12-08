810dcfa9687f3d3d3dc3ec7edcc4fd74fa3ad01d
nipun-jayawardhana <nipun.madhushanka1999@gmail.com>
2025-12-08 15:23:25 +0530

    vercel deployment improvements

--- backend/server.js
+++ backend/server.js
@@ -91,7 +91,7 @@ app.use((err, req, res, next) => {
 });
 
 const PORT = process.env.PORT || 5000;
-const HOST = process.env.HOST || '127.0.0.1';
+const HOST = process.env.HOST || '0.0.0.0';
 
 app.listen(PORT, HOST, () => {
   console.log('\n' + '='.repeat(50));
