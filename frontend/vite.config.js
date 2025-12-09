097a4ae53d9d68c800c1de89c3be84404c6b703d
Kavindi-Kinkini <10952821@students.plymouth.ac.uk>
2025-12-10 01:49:55 +0530

    update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000', // Change this to your backend port
+        target: 'http://localhost:5000', 
         changeOrigin: true,
         secure: false,
       }
